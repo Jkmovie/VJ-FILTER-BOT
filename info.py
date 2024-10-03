364587faca6dfcf7c918c2cf2fef3f119fbac991
Jkmovie <124867430+Jkmovie@users.noreply.github.com>
2024-10-03 12:47:35 +0530

    Update info.py

--- info.py
+++ info.py
@@ -31,7 +31,7 @@ MELCOW_VID = environ.get("MELCOW_VID", "https://t.me/jk_movie_addaa/11")
 SPELL_IMG = environ.get("SPELL_IMG", "https://te.legra.ph/file/15c1ad448dfe472a5cbb8.jpg")
 
 # Admins, Channels & Users
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '2175873848'))
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002175873848'))
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '6042310929').split()]
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002136011081').split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
